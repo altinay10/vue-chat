30efc618986dc2d6bebd5bd83a99a5115f00fa70
hectorpiece <efebarisaltinay@gmail.com>
2021-08-08 22:39:38 +0300

    Add profile color and improve design

--- src/components/message.vue
+++ src/components/message.vue
@@ -1,21 +1,71 @@
 <script>
 export default {
   props: {
-    Text,
+    message: Object,
+    lastUser: String,
   },
 };
 </script>
 
 <template>
   <div class="layout">
-    <p>{{ Text }}</p>
+    <div class="pp"></div>
+    <div class="message">
+      <div class="nick-time">
+        <div class="nick">{{ message.username }}</div>
+        <span class="date">{{ ("" + new Date()).substring(4, 25) }}</span>
+      </div>
+      <div class="text">{{ message.text }}</div>
+    </div>
   </div>
 </template>
 
 <style>
 .layout {
+  height: auto;
+  width: 96%;
+  margin: 2%;
+  display: flex;
+  justify-content: start;
   color: black;
   font-size: 18px;
-  margin: 15px;
+  text-align: start;
+}
+.pp {
+  height: 40px;
+  width: 40px;
+  border-radius: 100%;
+  background-color: purple;
+}
+.layout > .message {
+  max-width: 100%;
+  height: auto;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 3%;
+  padding: 0%;
+  text-align: start;
+}
+.nick-time {
+  width: auto;
+  display: flex;
+  justify-content: flex-start;
+  align-items: baseline;
+}
+.nick-time > .nick {
+  width: auto;
+  display: flex;
+  margin: 0% 12px 0% 0%;
+  font-size: 1em;
+  color: deeppink;
+  font-weight: bold;
+}
+.nick-time > .date {
+  font-size: 14px;
+}
+.message > .text {
+  word-break: break-all;
 }
 </style>
