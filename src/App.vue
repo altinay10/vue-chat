740598ac25c24b7dd584e10819aa8eec22e03c64
hectorpiece <efebarisaltinay@gmail.com>
2021-08-08 22:28:17 +0300

    Change font and improve css

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,49 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap");
+
 #app {
-  font-family: Whitney, "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+::-webkit-scrollbar {
+  width: 10px;
+}
+::-webkit-scrollbar-thumb {
+  background: crimson;
+  border-radius: 10px;
+}
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 15px gold;
+  border-radius: 10px;
+}
+::-webkit-scrollbar {
+  width: 10px;
+}
+::-webkit-scrollbar-thumb {
+  background: crimson;
+  border-radius: 10px;
+}
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 15px gold;
+  border-radius: 10px;
+}
 html {
   height: 100%;
   width: 100%;
+  font-family: "Roboto", sans-serif;
+}
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0px;
+  padding: 0px;
+  scroll-behavior: none;
+  display: flex;
+  justify-content: center;
 }
 </style>
